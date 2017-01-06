be96d62f0834b96ec26e05d91284d9c7d5458636
mitchellgoffpc <mitchellgoffpc@gmail.com>
2017-01-05 22:18:05 -0500

    Started building home page

--- SouffleMignon/urls.py
+++ SouffleMignon/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
+from home.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^blog/', include('blog.urls')),
+    url(r'^$', HomeView.as_view(), name='home')]
---
+++ home/static/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.search-btn').click(didClickSearch);
+});
+
+
+function didClickSearch () {
+    var hidden = $('.search-box').attr('data-hidden') == 'true';
+    $('.search-box').attr('data-hidden', hidden ? 'false' : 'true');
+    $('.search-box').animate({ marginRight: hidden ? 0 : -180 }, function() {
+        if (hidden) {
+            $('.search-box').focus();
+        } else {
+            $('.search-box').blur();
+        }
+    });
+}
